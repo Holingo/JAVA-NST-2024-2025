7104484c57da2667c24e0449b298dfb8e4abdd05
Oskar Świątek <pan.holingo@gmail.com>
2025-05-27 18:22:36 +0200

    feat: add `CarController` - admin endpoints for car management.

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/CarController.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/CarController.java
@@ -1,4 +1,44 @@
 package org.example.projectmanagerapp.controller;
 
+import org.example.projectmanagerapp.entity.car.Car;
+import org.example.projectmanagerapp.service.CarService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/cars")
+@PreAuthorize("hasRole('ADMIN')")
 public class CarController {
+    private CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable Long id) {
+        return carService.getCarById(id);
+    }
+
+    @PostMapping
+    public Car createCar(@RequestBody Car car) {
+        return carService.createCar(car);
+    }
+
+    @PutMapping("/{id}")
+    public Car updateCar(@PathVariable Long id, @RequestBody Car car) {
+        return carService.updateCar(id, car);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        carService.deleteCar(id);
+    }
 }
