d7c3111e476c495723579a1087371d0408689ae2
Oskar Świątek <pan.holingo@gmail.com>
2025-05-27 18:21:05 +0200

    feat: define `Car` entity for fleet management.

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/car/Car.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/car/Car.java
@@ -1,4 +1,29 @@
 package org.example.projectmanagerapp.entity.car;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cars")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false, unique = true)
+    private String registrationNumber;
+
+    @Column(nullable = false)
+    private boolean available = true;
 }
