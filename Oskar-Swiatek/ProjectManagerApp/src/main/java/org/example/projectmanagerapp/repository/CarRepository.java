79319c8cd4fcb69c3351846487b8403a4d34853d
Oskar Świątek <pan.holingo@gmail.com>
2025-05-27 18:22:12 +0200

    feat: create `CarRepository` with custom queries.

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/CarRepository.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/CarRepository.java
@@ -1,4 +1,12 @@
 package org.example.projectmanagerapp.repository;
 
-public interface CarRepository {
+import org.example.projectmanagerapp.entity.car.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+    Optional<Car> findByRegistrationNumber(String registrationNumber);
+    List<Car> findByAvailableTrue();
 }
