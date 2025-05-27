e28c3a0e1f4bb3a5993b41dd9c68ec63049425bf
Oskar Świątek <pan.holingo@gmail.com>
2025-05-27 18:23:34 +0200

    feat: implement `CustomUserDetailsService` - code beta - for Spring Security.

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/service/CustomUserDetailsService.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/service/CustomUserDetailsService.java
@@ -1,4 +1,29 @@
 package org.example.projectmanagerapp.service;
 
-public class CustomUserDetailsService {
+import org.example.projectmanagerapp.entity.user.User;
+import org.example.projectmanagerapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
 }
