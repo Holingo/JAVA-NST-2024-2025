91de63f0dec092833a405a6dbc3d1fb7d8421c35
Oskar Świątek <pan.holingo@gmail.com>
2025-05-28 17:11:58 +0200

    feat(address): embed `Address` object into `PrivateUser` and `CompanyUser` entities

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/common/Address.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/common/Address.java
@@ -1,4 +1,19 @@
 package org.example.projectmanagerapp.entity.common;
 
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
 public class Address {
+    private String houseNumber;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
 }
