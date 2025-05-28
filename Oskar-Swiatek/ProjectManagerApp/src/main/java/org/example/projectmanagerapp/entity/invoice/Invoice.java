f99f04aca98696d07cd960f49586ab9ce40b3ffb
Oskar Świątek <pan.holingo@gmail.com>
2025-05-28 17:19:15 +0200

    feat(invoice): connect `Invoice` entity with `CompanyUser` for future billing system

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/invoice/Invoice.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/invoice/Invoice.java
@@ -1,4 +1,36 @@
 package org.example.projectmanagerapp.entity.invoice;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.projectmanagerapp.entity.reservation.Reservation;
+import org.example.projectmanagerapp.entity.user.CompanyUser;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private LocalDate issuedDate;
+
+    private BigDecimal totalAmount;
+
+    private String invoiceNumber; // example: FV/2025/0001
+
+    @ManyToOne
+    @JoinColumn(name = "company_user_id")
+    private CompanyUser companyUser;
+
+    @OneToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
 }
