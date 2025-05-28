4f8bc623eed060930000c3f36f470d622fa2b397
Oskar Świątek <pan.holingo@gmail.com>
2025-05-28 17:18:35 +0200

    feat(controller): add `RegisterViewController` with form-based /register endpoint

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/RegisterViewController.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/RegisterViewController.java
@@ -1,4 +1,26 @@
 package org.example.projectmanagerapp.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.projectmanagerapp.dto.UserRegistrationDto;
+import org.example.projectmanagerapp.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class RegisterViewController {
+    private final UserService userService;
+
+    @GetMapping("/register")
+    public String showRegisterForm() {
+        return "register"; // resources/templates/register.html
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute UserRegistrationDto dto) {
+        userService.registerUser(dto);
+        return "redirect:/login"; // if success send to sign up
+    }
 }
