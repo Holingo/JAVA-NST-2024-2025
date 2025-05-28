9c4f0f615db67393c76fdf2d783890bc31eda026
Oskar Świątek <pan.holingo@gmail.com>
2025-05-28 12:52:14 +0200

    feat(reservation): create `Reservation` entity with `Car` and `User` relations

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/reservation/Reservation.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/reservation/Reservation.java
@@ -1,4 +1,39 @@
 package org.example.projectmanagerapp.entity.reservation;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.projectmanagerapp.entity.car.Car;
+import org.example.projectmanagerapp.entity.enums.ReservationStatus;
+import org.example.projectmanagerapp.entity.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Reservations")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @ManyToOne(optional = false)
+    private Car car;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReservationStatus status;
 }
