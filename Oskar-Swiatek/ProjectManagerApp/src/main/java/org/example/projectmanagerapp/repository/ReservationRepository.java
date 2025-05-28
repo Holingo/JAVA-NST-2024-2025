244ff8bb72ff125cfb13d1bc17f07bb0f114f3fd
Oskar Świątek <pan.holingo@gmail.com>
2025-05-28 12:52:54 +0200

    feat(reservation): add repository for `Reservation` entity

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ReservationRepository.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ReservationRepository.java
@@ -1,4 +1,16 @@
 package org.example.projectmanagerapp.repository;
 
-public interface ReservationRepository {
+import org.example.projectmanagerapp.entity.enums.ReservationStatus;
+import org.example.projectmanagerapp.entity.reservation.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByUserId(Long userId);
+
+    List<Reservation> findByUserIdAndStatus(Long userId, ReservationStatus status);
+
+    List<Reservation> findByStatus(ReservationStatus status);
 }
