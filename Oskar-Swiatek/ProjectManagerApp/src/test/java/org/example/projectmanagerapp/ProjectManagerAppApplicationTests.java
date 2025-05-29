1718951647a6dcc79169fe3e1374c6818aad3b3d
Oskar Świątek <pan.holingo@gmail.com>
2025-05-29 22:39:08 +0200

    chore(test): improving the performance of `Docker + JaCoCo`

--- Oskar-Swiatek/ProjectManagerApp/src/test/java/org/example/projectmanagerapp/ProjectManagerAppApplicationTests.java
+++ Oskar-Swiatek/ProjectManagerApp/src/test/java/org/example/projectmanagerapp/ProjectManagerAppApplicationTests.java
@@ -2,12 +2,32 @@ package org.example.projectmanagerapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ProjectManagerAppApplicationTests {
 
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("user")
+            .withPassword("pass");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @Test
     void contextLoads() {
+        // just check if context starts
     }
 
 }
